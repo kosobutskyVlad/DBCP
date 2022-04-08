3ecfd93362aca09aa673364e9c73496d770acbd9
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-04-08 22:58:12 +0300

    refactor: storetypes api pass data in body

--- server/endpoints/storetypes.py
+++ server/endpoints/storetypes.py
@@ -1,5 +1,12 @@
+from typing import Optional
+
 import pyodbc
+from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
+
+class StoreType(BaseModel):
+    storetype_id: str
+    storetype_description: Optional[str] = None
 
 router = APIRouter(
     prefix="/storetypes",
@@ -47,8 +54,8 @@ def get_storetype(storetype_id: str):
     raise HTTPException(status_code=404,
                         detail=f"{storetype_id} not found")
 
-@router.post("/add-storetype/{storetype_id}")
-def add_storetype(storetype_id: str, storetype_description: str):
+@router.post("/add-storetype")
+def add_storetype(storetype: StoreType):
 
     conn = pyodbc.connect(
         "Driver={SQL Server Native Client 11.0};"
@@ -58,7 +65,7 @@ def add_storetype(storetype_id: str, storetype_description: str):
 
     cursor = conn.cursor()
     cursor.execute(f"SELECT storetype_id FROM StoreTypes \
-                   WHERE storetype_id = '{storetype_id}'")
+                   WHERE storetype_id = '{storetype.storetype_id[:4]}'")
     data = []
     for row in cursor:
         data.append(list(row))
@@ -66,17 +73,20 @@ def add_storetype(storetype_id: str, storetype_description: str):
     if data:
         conn.close()
         raise HTTPException(status_code=422,
-                            detail=f"{storetype_id} already exists")
+                            detail=f"{storetype.storetype_id[:4]} \
+                            already exists")
 
     cursor.execute(f"INSERT INTO StoreTypes \
-                   VALUES('{storetype_id}', '{storetype_description}')")
+                   VALUES('{storetype.storetype_id[:4]}', \
+                   '{storetype.storetype_description[:100]}')")
     conn.commit()
     conn.close()
-    return {"storetype_id": storetype_id,
-            "storetype_description": storetype_description}
+    return {"storetype_id": storetype.storetype_id[:4],
+            "storetype_description":
+            storetype.storetype_description[:100]}
 
-@router.put("/update-storetype/{storetype_id}")
-def update_storetype(storetype_id: str, storetype_description: str):
+@router.put("/update-storetype")
+def update_storetype(storetype: StoreType):
 
     conn = pyodbc.connect(
         "Driver={SQL Server Native Client 11.0};"
@@ -86,7 +96,7 @@ def update_storetype(storetype_id: str, storetype_description: str):
 
     cursor = conn.cursor()
     cursor.execute(f"SELECT storetype_id FROM StoreTypes \
-                   WHERE storetype_id = '{storetype_id}'")
+                   WHERE storetype_id = '{storetype.storetype_id}'")
     data = []
     for row in cursor:
         data.append(list(row))
@@ -94,16 +104,18 @@ def update_storetype(storetype_id: str, storetype_description: str):
     if not data:
         conn.close()
         raise HTTPException(status_code=404,
-                            detail=f"{storetype_id} not found")
+                            detail=f"{storetype.storetype_id} \
+                            not found")
 
     cursor = conn.cursor()
     cursor.execute(f"UPDATE StoreTypes SET \
-                   storetype_description = '{storetype_description}' \
-                   WHERE storetype_id = '{storetype_id}'")
+                   storetype_description = \
+                   '{storetype.storetype_description[:100]}' \
+                   WHERE storetype_id = '{storetype.storetype_id}'")
     conn.commit()
 
     cursor.execute(f"SELECT * FROM StoreTypes \
-                   WHERE storetype_id = '{storetype_id}'")
+                   WHERE storetype_id = '{storetype.storetype_id}'")
     data = []
     for row in cursor:
         data.append(list(row))
