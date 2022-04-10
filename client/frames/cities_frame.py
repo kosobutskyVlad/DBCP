6c17e05718b6d99584410b3228de51c99fa2397c
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-04-10 22:07:13 +0300

    style: line length pep 8

--- client/frames/cities_frame.py
+++ client/frames/cities_frame.py
@@ -1,4 +1,3 @@
-from regex import S
 import requests
 
 import imgui
@@ -82,25 +81,33 @@ def cities_frame(host: str, port: int):
                     f"http://{host}:{port}/cities/get-city/{city}")
                 #imgui.begin_popup_modal()
                 info = get_city_response.json()["Data"][0]
-                cities_info[city] = {"city_name": info[1][:50], "city_size": info[2][:10], "country": info[3][:50]}
+                cities_info[city] = {"city_name": info[1][:50],
+                                     "city_size": info[2][:10],
+                                     "country": info[3][:50]}
             
             imgui.begin_child("cities_editor", 1200, 200, border=True)
             imgui.text(city)
             imgui.same_line()
             imgui.push_item_width(300)
-            changed, cities_info[city]["city_name"] = imgui.input_text(f"{city}: city_name", cities_info[city]["city_name"], 51)
+            changed, cities_info[city]["city_name"] = \
+                imgui.input_text(f"{city}: city_name",
+                cities_info[city]["city_name"], 51)
             if changed:
                 cities_changed[city] = True
             imgui.pop_item_width()
             imgui.same_line()
             imgui.push_item_width(100)
-            changed, cities_info[city]["city_size"] = imgui.input_text(f"{city}: city_size", cities_info[city]["city_size"], 11)
+            changed, cities_info[city]["city_size"] = \
+                imgui.input_text(f"{city}: city_size",
+                cities_info[city]["city_size"], 11)
             if changed:
                 cities_changed[city] = True
             imgui.pop_item_width()
             imgui.same_line()
             imgui.push_item_width(300)
-            changed, cities_info[city]["country"] = imgui.input_text(f"{city}: country", cities_info[city]["country"], 51)
+            changed, cities_info[city]["country"] = \
+                imgui.input_text(f"{city}: country",
+                cities_info[city]["country"], 51)
             if changed:
                 cities_changed[city] = True
             imgui.pop_item_width()
@@ -111,21 +118,28 @@ def cities_frame(host: str, port: int):
         button_clicked_update_cities = False
         for city in cities_changed:
             if cities_changed[city]:
-                response_update_city = requests.put(f"http://{host}:{port}/cities/update-city", json={"city_id": city, "city_name": cities_info[city]["city_name"], "city_size": cities_info[city]["city_size"], "country": cities_info[city]["country"]})
+                response_update_city = requests.put(
+                    f"http://{host}:{port}/cities/update-city",
+                    json={"city_id": city,
+                    "city_name": cities_info[city]["city_name"],
+                    "city_size": cities_info[city]["city_size"],
+                    "country": cities_info[city]["country"]})
 
     button_clicked_delete_cities = imgui.button("Delete cities")
     if button_clicked_delete_cities:
         button_clicked_delete_cities = False
         for city in selectable_cities:
             if selectable_cities[city]:
-                response_delete_city = requests.delete(f"http://{host}:{port}/cities/delete-city/{city}")
+                response_delete_city = requests.delete(
+                    f"http://{host}:{port}/cities/delete-city/{city}")
                 if response_delete_city.status_code == 409:
                     show_popup_delete_error = True
     
     if show_popup_delete_error:
         imgui.open_popup("Integrity Error")
     if imgui.begin_popup_modal("Integrity Error")[0]:
-        imgui.text(f"Record {city} could not be deleted because it is being referenced by a foreign key.")
+        imgui.text(f"Record {city} could not be deleted because \
+                   it is being referenced by a foreign key.")
 
         if imgui.button(label="Close"):
             imgui.close_current_popup()
@@ -133,25 +147,30 @@ def cities_frame(host: str, port: int):
         imgui.end_popup()
 
     imgui.push_item_width(50)
-    _, info_add_city["city_id"] = imgui.input_text("Add city_id", info_add_city["city_id"], 5)
+    _, info_add_city["city_id"] = imgui.input_text(
+        "Add city_id", info_add_city["city_id"], 5)
     imgui.pop_item_width()
     imgui.same_line()
     imgui.push_item_width(300)
-    _, info_add_city["city_name"] = imgui.input_text(f"Add city_name", info_add_city["city_name"], 51)
+    _, info_add_city["city_name"] = imgui.input_text(
+        f"Add city_name", info_add_city["city_name"], 51)
     imgui.pop_item_width()
     imgui.same_line()
     imgui.push_item_width(100)
-    _, info_add_city["city_size"] = imgui.input_text(f"Add city_size", info_add_city["city_size"], 11)
+    _, info_add_city["city_size"] = imgui.input_text(
+        f"Add city_size", info_add_city["city_size"], 11)
     imgui.pop_item_width()
     imgui.same_line()
     imgui.push_item_width(300)
-    _, info_add_city["country"] = imgui.input_text(f"Add country", info_add_city["country"], 51)
+    _, info_add_city["country"] = imgui.input_text(
+        f"Add country", info_add_city["country"], 51)
     imgui.pop_item_width()
     imgui.same_line()
 
     button_clicked_add_city = imgui.button("Add a city")
     if button_clicked_add_city:
         button_clicked_add_city = False
-        response_add_city = requests.post(f"http://{host}:{port}/cities/add-city", json=info_add_city)
+        response_add_city = requests.post(
+            f"http://{host}:{port}/cities/add-city", json=info_add_city)
 
     imgui.end()
