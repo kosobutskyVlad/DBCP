23d23f8cfb4f873039c7ef2f06d83280f9cbd2c8
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-04-14 17:35:12 +0300

    refactor: rework erro popup

---
+++ client/frames/error_popup.py
@@ -0,0 +1,13 @@
+import imgui
+
+def error_popup(show: bool, error_message: str):
+    if show:
+        imgui.open_popup("Error")
+    if imgui.begin_popup_modal("Error")[0]:
+        imgui.text(error_message)
+
+        if imgui.button(label="Close"):
+            imgui.close_current_popup()
+            show = False
+        imgui.end_popup()
+    return show
