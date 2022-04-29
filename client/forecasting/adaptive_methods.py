2973cb8bb208fbcb21cf04e84160e68cd7a7821c
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-04-29 16:04:47 +0300

    feat: optimize alpha

--- client/forecasting/adaptive_methods.py
+++ client/forecasting/adaptive_methods.py
@@ -1,3 +1,7 @@
+from itertools import repeat
+
+import numpy as np
+
 def get_loss(y_true, y_pred, params):
     offset = y_pred - y_true
     if offset < params[0]:
@@ -18,12 +22,35 @@ def get_loss(y_true, y_pred, params):
 
 class EMA():
     alpha = 0
-    y_pred = 0
+    y_prev = 0
 
-    def __init__(self, alpha, y_pred):
+    def __init__(self, alpha, y_prev):
         self.alpha = alpha
-        self.y_pred = y_pred
+        self.y_prev = y_prev
 
     def predict(self, y_true):
-        self.y_pred = self.alpha * y_true + (1-self.alpha) * self.y_pred
-        return self.y_pred
+        self.y_prev = self.alpha*y_true + (1-self.alpha)*self.y_prev
+        return self.y_prev
+
+    def fit(self, y, loss_params):
+        best_loss = np.inf
+        best_alpha = 0.01
+    
+        for alpha in np.arange(0.01, 1, 0.01):
+            y_pred = []
+            self.alpha = alpha
+            self.y_prev = y[0]
+            
+            for y_true in y[:-1]:
+                y_pr = self.predict(y_true)
+                y_pred.append(y_pr)
+            
+            loss = sum(map(
+                get_loss, y[1:], y_pred, repeat(loss_params))
+            ) / len(y - 1)
+            
+            if best_loss > loss:
+                best_loss = loss
+                best_alpha = alpha
+        
+        self.alpha = best_alpha
