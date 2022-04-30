f67be9a1bd0ef925de5208fd8ca65721de59cbb2
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-04-30 09:20:09 +0300

    feat: Holts model

--- client/forecasting/adaptive_methods.py
+++ client/forecasting/adaptive_methods.py
@@ -21,12 +21,14 @@ def get_loss(y_true, y_pred, params):
             + (offset - params[5])*(params[2] + params[4] + params[6]))
 
 class EMA():
-    alpha = 0
+    alpha = 0.01
     y_prev = 0
 
-    def __init__(self, alpha, y_prev):
-        self.alpha = alpha
-        self.y_prev = y_prev
+    def __init__(self, alpha=None, y_prev=None):
+        if alpha is not None:
+            self.alpha = alpha
+        if y_prev is not None:
+            self.y_prev = y_prev
 
     def predict(self, y_true):
         self.y_prev = self.alpha*y_true + (1-self.alpha)*self.y_prev
@@ -54,3 +56,52 @@ class EMA():
                 best_alpha = alpha
         
         self.alpha = best_alpha
+
+class Holts():
+    alpha1 = 0.01
+    alpha2 = 0.01
+    a = 0
+    b = 0
+
+    def __init__(self, alpha1=None, alpha2=None, a=None, b=None):
+        if alpha1 is not None:
+            self.alpha1 = alpha1
+        if alpha2 is not None:
+            self.alpha2 = alpha2
+        if a is not None:
+            self.a = a
+        if b is not None:
+            self.b = b
+
+    def predict(self, y_true, dist=1):
+        a_prev = self.a
+        self.a = self.alpha1*y_true + (1-self.alpha1)*(self.a-self.b)
+        self.b = self.alpha2*(self.a-a_prev) + (1-self.alpha2)*self.b
+        return self.a + self.b*dist
+
+    def fit(self, y, loss_params):
+        best_loss = np.inf
+        best_alpha = (0.01, 0.01)
+
+        for alpha1 in np.arange(0.01, 1, 0.01):
+            for alpha2 in np.arange(0.01, 1, 0.01):
+                y_pred = []
+                self.alpha1 = alpha1
+                self.alpha2 = alpha2
+                self.a = y[0]/2
+                self.b = y[0]/2
+
+                for y_true in y[:-1]:
+                    y_pr = self.predict(y_true)
+                    y_pred.append(y_pr)
+
+                loss = sum(map(
+                    get_loss, y[1:], y_pred, repeat(loss_params))
+                ) / len(y - 1)
+
+                if best_loss > loss:
+                    best_loss = loss
+                    best_alpha = alpha1, alpha2
+
+        self.alpha1 = best_alpha[0]
+        self.alpha2 = best_alpha[1]
