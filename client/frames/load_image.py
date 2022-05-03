c71de9de10684755ec679ffbb91f7c7ce9a68843
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-05-03 14:57:35 +0300

    refactor: split creating and registering image

--- client/frames/load_image.py
+++ client/frames/load_image.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import OpenGL.GL as gl
 
-def get_textureID(sales, prediction_results):
-
+def create_image(purchases_df, prediction_results):
     plt.plot(prediction_results, label="predicted")
-    plt.plot(sales, alpha=0.6, label="true")
+    plt.plot(purchases_df["sales"].values, alpha=0.6, label="true")
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.legend(fontsize=14)
     plt.savefig('prediction.jpg')
 
+def get_textureID():
     image = Image.open("prediction.jpg") 
     image = image.convert('RGB')   
     img_data = np.array(list(image.getdata()), np.uint8)
@@ -25,7 +25,10 @@ def get_textureID(sales, prediction_results):
     gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_BASE_LEVEL, 0)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAX_LEVEL, 0)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, image.size[0], image.size[1], 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, img_data)
+    gl.glTexImage2D(
+        gl.GL_TEXTURE_2D, 0, gl.GL_RGB, image.size[0],
+        image.size[1], 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, img_data
+    )
     image.close()
     
     return textureID, image.size
