4e135655d0145776dbbb26c2b7524275c82e17cd
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-05-05 17:56:52 +0300

    feat: init store node window

--- store/store_window.py
+++ store/store_window.py
@@ -4,7 +4,7 @@ import OpenGL.GL as gl
 import imgui
 
 from render_utils import start_window
-
+from xml_generation.procurement_plan import generate_plan
 
 server_host = "127.0.0.1"
 server_port = "5000"
@@ -12,14 +12,18 @@ server_port = "5000"
 forecast_host = "127.0.0.1"
 forecast_port = "5001"
 
-def frame_server():
+store_id = ""
+
+def frame_store():
 
     global server_host
     global server_port
 
     global forecast_host
     global forecast_port
-    
+
+    global store_id
+
     gl.glClearColor(0.1, 0.1, 0.1, 1)
     gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
@@ -48,9 +52,18 @@ def frame_server():
 
     imgui.end()
 
+    _, store_id = imgui.input_text("Store ID", store_id, 256)
+
+    if imgui.button("Generate procurement plan"):
+        generate_plan(
+            server_host, server_port,
+            forecast_host, forecast_port,
+            store_id
+        )
+
 
 def main():
-    start_window(300, 200, "Sales Forecasting: Server", frame_server)
+    start_window(300, 200, "Sales Forecasting: Server", frame_store)
 
 
 if __name__ == '__main__':
