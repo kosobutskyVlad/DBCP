c7ce14343f874edd5ec6b315d80080f4c23addaa
Vlad Kosobutsky <vlad.kosobutsky@gmail.com>
2022-05-28 16:08:08 +0300

    docs: add type hints

--- forecast/forecasting/loss_function.py
+++ forecast/forecasting/loss_function.py
@@ -1,4 +1,6 @@
-def get_loss(y_true, y_pred, params):
+from typing import List
+
+def get_loss(y_true: float, y_pred: float, params: List[float]) -> float:
     offset = y_pred - y_true
     if offset < params[0]:
         return (offset-params[0])*params[1]
@@ -16,17 +18,18 @@ def get_loss(y_true, y_pred, params):
             + (params[5] - params[3])*(params[2] + params[4])
             + (offset - params[5])*(params[2] + params[4] + params[6]))
 
-def get_loss_derivative(y_true, y_pred, parameters):
+def get_loss_derivative(y_true: float, y_pred: float,
+                        params: List[float]) -> float:
     offset = y_pred - y_true
-    if offset < parameters[0]:
-        return parameters[1]
+    if offset < params[0]:
+        return params[1]
     if offset < 0:
         return 0.01
     
-    if offset < parameters[3]:
-        return parameters[2]
+    if offset < params[3]:
+        return params[2]
     
-    if offset < parameters[5]:
-        return parameters[2] + parameters[4]
+    if offset < params[5]:
+        return params[2] + params[4]
     
-    return parameters[2] + parameters[4] + parameters[6]
+    return params[2] + params[4] + params[6]
