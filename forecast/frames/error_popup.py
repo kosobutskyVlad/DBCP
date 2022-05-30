15d57ac8a930738d52d434b7ae4473fbe3e3f207
kosobutskyVlad <vlad.kosobutsky@gmail.com>
2022-05-30 16:19:41 +0300

    fix: process pydantic 422 response

--- forecast/frames/error_popup.py
+++ forecast/frames/error_popup.py
@@ -4,6 +4,7 @@ def error_popup(show: bool, error_message: str) -> bool:
     if show:
         imgui.open_popup("Error")
     if imgui.begin_popup_modal("Error")[0]:
+        error_message = process_error_message(error_message)
         imgui.text(error_message)
 
         if imgui.button(label="Close"):
@@ -11,3 +12,13 @@ def error_popup(show: bool, error_message: str) -> bool:
             show = False
         imgui.end_popup()
     return show
+
+def process_error_message(error_message) -> str:
+    if isinstance(error_message, str):
+        return error_message
+
+    errors = []
+    for error in error_message:
+        errors.append(f"{error['loc'][1]}: {error['msg']}")
+
+    return "\n".join(errors)
